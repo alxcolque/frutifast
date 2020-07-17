1ea2ae1ac50601b73fac3126a6867df1ff015824
alxcolque <a.colq.c@hotmail.com>
2020-07-17 14:33:19 -0400

    Se agregó las rutas

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 
 // Importing routes
 
@@ -9,20 +10,34 @@ const app = express();
 //setings
 
 app.set('port', process.env.PORT || 4000);
-app.set('view engine', 'ejs')
+
 app.set('views',path.join(__dirname,'views'));
+
+app.engine('.hbs', exphbs({
+	defaultLayout: 'main',
+	layoutsDir: path.join(app.get('views'), '/layouts'),
+	partialsDir: path.join(app.get('views'), '/partials'),
+	extname: '.hbs',
+	helpers: require('./helpers/handlebars')
+}));
+
+app.set('view engine','.hbs');
 
 //Midlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
-
+app.use(express.json());
 
 //Global Variables
-
+app.use((req,res,next)=>{
+	next();
+})
 
 //Routes
 
 app.use(require('./routes/index.routes'));
+app.use(require('./routes/auth.routes'));
+app.use('/links',require('./routes/links.routes'));
 
 // Static Files
 app.use(express.static(path.join(__dirname,'public')));
