dab422a97362e391bbdf3ffaed9c3d66dd544e27
alxcol <a.colq.c@hotmail.com>
2020-08-20 16:35:04 -0400

    Eliminado Duplicados

--- frutifast/src/index.js
+++
@@ -1,6 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const app = require("./server");
-
-app.listen(app.get("port"));
-console.log("Server is in port", app.get("port"));
--- frutifast/src/routes/index.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { renderIndex } = require("../controllers/index.conroller");
-
-router.get("/", renderIndex);
-
-module.exports = router;
